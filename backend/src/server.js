f5fe370037fb2150e3af0eefba689be1c1635a81
Anisha1903 <anisham0219@gmail.com>
2023-12-05 15:38:24 +0530

    First commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ app.get("*", (req, res) => {
   res.sendFile(indexFilePath);
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
 });
